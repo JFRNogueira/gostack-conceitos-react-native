c2680841f1766f405952553626afd8646639ff0f
johannes <johannes.nogueira@eduq.app>
2020-05-03 21:21:54 -0300

    challenge solution

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  //baseURL: 'http://189.47.64.227:3333'
+  baseURL: 'http://10.0.2.2:4444',
+  //baseURL: 'http://localhost:3333/'
 });
 
 export default api;
